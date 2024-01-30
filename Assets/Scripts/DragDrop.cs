d5bf06bf51d6dac4892f809f95362ad6602ce514
Jess Woodward <JW1519@student.aru.ac.uk>
2024-01-30 20:22:31 +0000

    draging

---
+++ Assets/Scripts/DragDrop.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDrop : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+
+    Transform parentAfterDrag;
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("Begin");
+        parentAfterDrag = transform.parent;
+        transform.SetParent(transform.root);
+        transform.SetAsLastSibling();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("Drag");
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("End");
+        transform.SetParent(parentAfterDrag);
+    }
+}
