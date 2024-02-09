37fedb1549a3459ab70415483fe625e2bf6df1ad
Jess Woodward <JW1519@student.aru.ac.uk>
2024-02-09 13:00:42 +0000

    g

--- Assets/Scripts/PowerUps.cs
+++ Assets/Scripts/PowerUps.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PowerUps : MonoBehaviour
+public class PwerUpsDoubleDamage : MonoBehaviour
 {
 
     public void OnButtonPress()
