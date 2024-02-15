942e6b3a74fa65c38d588859476b8a63cb6ae7ee
Jess Woodward <JW1519@student.aru.ac.uk>
2024-02-15 14:08:08 +0000

    h

--- Assets/Scripts/PannelObjectInteraction.cs
+++ Assets/Scripts/PannelObjectInteraction.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PannelObjectInteraction : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        // Check if the left mouse button was clicked
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Check if the mouse was clicked over a UI element
+            if (EventSystem.current.Is())
+            {
+                Debug.Log("Clicked on the UI");
+            }
+        }
     }
 }
