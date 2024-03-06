288cea672e22fd6322772cb914c2750bc5257b81
Jess Woodward <JW1519@student.aru.ac.uk>
2024-03-06 11:00:16 +0000

    Ui colour changing

--- Assets/Scripts/ChangeUIColour.cs
+++ Assets/Scripts/ChangeUIColour.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
 
 public class ChangeUIColour : MonoBehaviour
 {
+
     public Image ArcherSlot;
     public Image CannonSlot;
     public Image MagicSlot;
+
+    public Color CantUseButton = Color.red;
+    public Color CanUseButton = Color.green;
 
     private void Awake()
     {
@@ -18,17 +22,31 @@ public class ChangeUIColour : MonoBehaviour
     {
         if (Gold.instance.GoldAmount < 500)
         {
-            MagicSlot.tintColor = Color.red;
+            MagicSlot.color = CantUseButton;
 
             if (Gold.instance.GoldAmount < 300)
             {
-                CannonSlot.tintColor = Color.red;
+                CannonSlot.color = CantUseButton;
 
                 if (Gold.instance.GoldAmount < 200)
                 {
-                    ArcherSlot.tintColor = Color.red;
+                    ArcherSlot.color = CantUseButton;
                 }
             }
         }
+        if (Gold.instance.GoldAmount >= 200)
+        {
+            ArcherSlot.color = CanUseButton;
+
+            if (Gold.instance.GoldAmount >= 300)
+            {
+                CannonSlot.color = CanUseButton;
+
+                if (Gold.instance.GoldAmount >= 500)
+                {
+                    MagicSlot.color = CanUseButton;
+                }
+            }
+        }         
     }
 }
