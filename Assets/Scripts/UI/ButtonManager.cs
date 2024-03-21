d40d4118b576ab19015a924d0b611abbbe86e1f3
Jess Woodward <JW1519@student.aru.ac.uk>
2024-03-21 18:46:39 +0000

    f

--- Assets/Scripts/UI/ButtonManager.cs
+++ Assets/Scripts/UI/ButtonManager.cs
@@ -19,8 +19,6 @@ public class ButtonManager : MonoBehaviour
                 StartCoroutine(child.DoubleDamageDuration());
             }
         }
-        
-
     }
     public void OnFreezeButtonPressed()
     {
